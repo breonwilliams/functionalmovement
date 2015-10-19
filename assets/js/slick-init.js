c98b233c515a2584a929df5e790678b544de7b9f
breonw <breon_williams@unc.edu>
2015-10-18 20:04:20 -0400

    modify files

--- assets/js/slick-init.js
+++ assets/js/slick-init.js
@@ -27,4 +27,28 @@ $(document).ready(function() {
             }
         }]
     });
+
+    $('.vid-carousel').slick({
+        dots: true,
+        arrows: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 2000,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
 });
