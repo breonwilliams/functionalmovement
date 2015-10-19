8c52b66760cd97c4c19115ab89fa3883867a4af6
breonw <breon_williams@unc.edu>
2015-10-18 20:09:59 -0400

    modify styles

--- assets/js/slick-init.js
+++ assets/js/slick-init.js
@@ -33,13 +33,13 @@ $(document).ready(function() {
         arrows: false,
         infinite: false,
         speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         responsive: [{
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
             }
