dfaf0b1532f067e8acff3baa0ec38aea891d0650
breonwilliams <bjwilli83@gmail.com>
2015-12-22 11:52:21 -0500

    modified styles and menu

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -114,3 +114,22 @@ $(function() {
 
     });
 });
+
+// append modal to body
+
+$(function() {
+    var checkeventcount = 1,prevTarget;
+    $('.modal').on('show.bs.modal', function (e) {
+        if(typeof prevTarget == 'undefined' || (checkeventcount==1 && e.target!=prevTarget))
+        {
+            prevTarget = e.target;
+            checkeventcount++;
+            e.preventDefault();
+            $(e.target).appendTo('body').modal('show');
+        }
+        else if(e.target==prevTarget && checkeventcount==2)
+        {
+            checkeventcount--;
+        }
+    });
+});
