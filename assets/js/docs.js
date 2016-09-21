402cf4be5694e3f7ff0187cfcdc1a9efb418ff7d
breonwilliams <bjwilli83@gmail.com>
2016-09-21 08:59:35 -0400

    assets/js/docs.js

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -133,3 +133,18 @@ $(function() {
         }
     });
 });
+
+// Smooth scroll to div
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    });
+});
