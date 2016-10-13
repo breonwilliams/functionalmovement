95533dace91e596b58b55a6a844b9f62bd6d5b16
breonwilliams <bjwilli83@gmail.com>
2016-10-13 06:04:14 -0400

    modify data tables

--- assets/js/datatables-init.js
+++ assets/js/datatables-init.js
@@ -1,45 +1,20 @@
 $(document).ready(function() {
-    var table = $('#dataTablee').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-            'copy', {
-                extend: 'csv',
-                exportOptions: {
-                    format: {
-                        body: function(data, row, column) {
-                            var result = data;
-                            var cols = [2];
-                            var field_separator = ",";
-                            if ($.inArray(column, cols) > -1) {
-                                tokens = data.split(/["']/);
-                                url = tokens[1].replace(",", "%2c");
-                                link_text = tokens[2].replace("</a>", "").replace(">", "").replace("\"", "'");
-                                result = "=HYPERLINK(\"" + url + "\"" + field_separator + "\"" + link_text + "\")";
-                            }
-                            return result;
-                        }
-                    }
-                }
-            },
-            'excel',
-            'pdf',
-            'print'
-        ]
-    });
-    // Set OnClick Event
-    $('#example tbody').on('click', 'tr', function() {
-        if ($(this).hasClass('selected')) {
-            $(this).removeClass('selected');
-        } else {
-            table.$('tr.selected').removeClass('selected');
-            $(this).addClass('selected');
+    //Only needed for the filename of export files.
+    //Normally set in the title tag of your page.
+    document.title='Simple DataTable';
+    // DataTable initialisation
+    $('#dataTablee').DataTable(
+        {
+            "dom": '<"dt-buttons"Bf><"clear">lirtp',
+            "paging": true,
+            "autoWidth": true,
+            "buttons": [
+                'copyHtml5',
+                'csvHtml5',
+                'excelHtml5',
+                'pdfHtml5',
+                'print'
+            ]
         }
-    });
-    // Set OnMouseEnter Event
-    $('#example tbody').on('mouseenter', 'td', function() {
-        var colIdx = table.cell(this).index().column;
-
-        $(table.cells().nodes()).removeClass('selected');
-        $(table.column(colIdx).nodes()).addClass('selected');
-    });
+    );
 });
