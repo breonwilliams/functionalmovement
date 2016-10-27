9d55f5457710c3d843273fe396409cfe75ebdf4d
breonwilliams <bjwilli83@gmail.com>
2016-10-27 13:37:04 -0400

    modify page header

--- assets/js/jquery.background-video.js
+++ assets/js/jquery.background-video.js
@@ -213,7 +213,7 @@
 		fadeOnEnd: true, // When we've reached the pauseAfter time
 		showPausePlay: true, // Show pause/play button
 		pausePlayXPos: 'right', // left|right|center
-		pausePlayYPos: 'top', // top|bottom|center
+		pausePlayYPos: 'bottom', // top|bottom|center
 		pausePlayXOffset: '15px', // pixels or percent from side - ignored if positioned center
 		pausePlayYOffset: '15px' // pixels or percent from top/bottom - ignored if positioned center
 	};
