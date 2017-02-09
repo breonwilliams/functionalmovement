5ddea067b844331b514bd6b4d9d50113b106a919
breonwilliams <bjwilli83@gmail.com>
2017-02-09 09:44:23 -0500

    modify text

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -57,6 +57,19 @@ $(function () {
     });
 })
 
+$(function() {
+    // Javascript to enable link to tab
+    var hash = document.location.hash;
+    var prefix = "tab_";
+    if (hash) {
+        $('.nav-tabs a[href=' + hash.replace(prefix, "") + ']').tab('show');
+    }
+
+    // Change hash for page-reload
+    $('.nav-tabs a').on('shown', function(e) {
+        window.location.hash = e.target.hash.replace("#", "#" + prefix);
+    });
+});
 // Sticky Nav
 
     $(function() {
